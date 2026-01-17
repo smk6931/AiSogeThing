b4b6e65b15875e8dfdd2e48270226c784465c14a
Smk <thsaudrhks1@gmail.com>
2026-01-17 18:14:08 +0900

    리엑트 모바일 / 데스크탑 분기 과정 추가

--- front/src/api/client.js
+++ front/src/api/client.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 // 백엔드 기본 URL 설정
 const client = axios.create({
-  baseURL: 'http://localhost:8001',
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8001',
   headers: {
     'Content-Type': 'application/json',
